713afc4048864a809225553bc1b9f0617bddfefb
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-15 22:58:39 +0300

    update browser setup

--- Client-side/src/App.jsx
+++ Client-side/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
@@ -11,7 +12,10 @@ const App = () => {
         <CartProvider>
           <div className="min-h-screen bg-[#FFF9F6]">
             <Navbar />
-            {/* Routes will be added here */}
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* Other routes will be added here */}
+            </Routes>
           </div>
         </CartProvider>
       </AuthProvider>
