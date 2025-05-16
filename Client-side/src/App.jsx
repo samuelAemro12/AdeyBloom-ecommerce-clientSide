12dffcbeee32a58e937670349b51073d70228902
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-16 22:13:45 +0300

    rendering bug fixed

--- Client-side/src/App.jsx
+++ Client-side/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import Navbar from './components/Navbar';
@@ -7,19 +7,17 @@ import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <CartProvider>
-          <div className="min-h-screen bg-[#FFF9F6]">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              {/* Other routes will be added here */}
-            </Routes>
-          </div>
-        </CartProvider>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <CartProvider>
+        <div className="min-h-screen bg-[#FFF9F6]">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* Other routes will be added here */}
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
   );
 };
 
