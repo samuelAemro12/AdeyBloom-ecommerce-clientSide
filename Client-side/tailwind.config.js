2a74791f739a6bbbf33978978405ca073b7ab9cb
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-17 00:44:09 +0300

    update color pallet

--- Client-side/tailwind.config.js
+++ Client-side/tailwind.config.js
@@ -7,10 +7,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#C585D7',
-        secondary: '#008080',
-        background: '#FFF9F6',
-        text: '#2F2F2F'
+        'primary-accent': '#C585D7',
+        'secondary-accent': '#FFDDE2',
+        'background': '#FFF9F6',
+        'cloud-gray': '#D6D6D6',
+        'ash-gray': '#6A6A6A',
+        'error': '#F47C7C',
+        'error-light': '#FEE2E2',
+        'error-dark': '#B91C1C',
       }
     },
   },
