e096852cad31c1f405c34d7e3989570a855f62a3
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-05-27 00:22:26 +0300

    wishlist context provider added

--- Client-side/src/layouts/RootLayout.jsx
+++ Client-side/src/layouts/RootLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { AuthProvider } from '../context/AuthContext';
 import { CartProvider } from '../context/CartContext';
 import { UserProvider } from '../context/UserContext';
+import { WishlistProvider } from '../context/WishlistContext';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -10,13 +11,15 @@ const RootLayout = () => {
     <AuthProvider>
       <UserProvider>
         <CartProvider>
-          <div className="min-h-screen bg-[#FFF9F6] flex flex-col">
-            <Navbar />
-            <main className="flex-grow">
-              <Outlet />
-            </main>
-            <Footer />
-          </div>
+          <WishlistProvider>
+            <div className="min-h-screen bg-[#FFF9F6] flex flex-col">
+              <Navbar />
+              <main className="flex-grow">
+                <Outlet />
+              </main>
+              <Footer />
+            </div>
+          </WishlistProvider>
         </CartProvider>
       </UserProvider>
     </AuthProvider>
